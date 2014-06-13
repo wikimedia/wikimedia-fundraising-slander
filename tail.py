6713f404b6187181870551c81b93e0f19afa23eb
Adam Roses Wight <awight@wikimedia.org>
2014-06-13 16:34:05 -0700

    deal with logfile rotation
    Change-Id: Ie3904a4211371437c82e633ffa09c73a7ed54a6e

--- tail.py
+++ tail.py
@@ -1,3 +1,6 @@
+import os
+import os.path
+
 import log
 
 
@@ -8,10 +11,44 @@ class TailPoller(object):
         log.info("Initializing file tailer on: [{path}]".format(path=path))
 
         self.path = path
-        self.file = file(path, "r")
+        self.inode = os.stat(self.path).st_ino
+        self.file = None
+
+        self.reload()
 
         # fast-forward to the end-of-file
         self.file.seek(0, 2)
 
+        self.output_buffer = []
+
+    def reload(self):
+        """Check the file status and reopen if we have been rotated out"""
+
+        if self.file:
+            if os.stat(self.path).st_ino != self.inode:
+                log.info("Logfile has rotated!  Refreshing...")
+                # dump any new lines before closing the old file
+                self.read()
+                self.file = None
+
+        if not self.file:
+            self.file = file(self.path, "r")
+            self.inode = os.fstat(self.file.fileno()).st_ino
+
     def check(self):
-        return [l.strip() for l in self.file.readlines()]
+        """Check for logfile rotation, return all new lines"""
+
+        try:
+            self.reload()
+            self.read()
+        except IOError as ex:
+            log.error(ex)
+            return
+
+        stripped = [l.strip() for l in self.output_buffer]
+        self.output_buffer = []
+        return stripped
+
+    def read(self):
+        """Pull line to end-of-file and store"""
+        self.output_buffer.extend(self.file.readlines())
